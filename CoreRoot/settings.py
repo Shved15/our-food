ec65ebc9466945e4fd41be430770070fff508424
Shved15 <maks.shihiwtsow15@gmail.com>
2023-05-06 21:50:52 +0200

    "add media files configurations"

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -150,6 +150,10 @@ STATICFILES_DIRS = [
     'CoreRoot/static',
 ]
 
+# Media files configurations
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
