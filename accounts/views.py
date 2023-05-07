c1e011777008932090bba51bcda3e89391b22946
Shved15 <maks.shihiwtsow15@gmail.com>
2023-05-07 12:22:01 +0200

    "added user registration form"

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from accounts.forms import UserForm
+
 
 def register_user(request):
-    return HttpResponse('This is user registration form')
+    form = UserForm
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/register-user.html', context)
