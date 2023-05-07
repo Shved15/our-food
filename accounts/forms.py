3b0211a6e79f01f6805213e07841e6ffb8c2d428
Shved15 <maks.shihiwtsow15@gmail.com>
2023-05-07 12:47:58 +0200

    "modified view for registration form"

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,9 +4,22 @@ from accounts.models import User
 
 
 class UserForm(forms.ModelForm):
+    """A form for creating a new user, with password confirmation."""
     password = forms.CharField(widget=forms.PasswordInput())
     confirm_password = forms.CharField(widget=forms.PasswordInput())
 
+    # def clean(self):
+    #     """Custom form validation method to check if the password and confirm password fields match."""
+    #     cleaned_data = super(UserForm, self).clean()
+    #     password = cleaned_data.get('password')
+    #     confirm_password = cleaned_data.get('confirm_password')
+    #
+    #     if password != confirm_password:
+    #         raise forms.ValidationError(
+    #             "Password does not match!"
+    #         )
+
     class Meta:
+        """ Model metadata for UserForm."""
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,20 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from accounts.forms import UserForm
+from accounts.models import User
 
 
 def register_user(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.role = User.CUSTOMER
+            user.save()
+            return redirect('register_user')
+    else:
+        form = UserForm()
     form = UserForm
     context = {
         'form': form
