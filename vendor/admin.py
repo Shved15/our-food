5625a5b8eb621c4ec08fde8875d4e20643579a39
Shved15 <maks.shihiwtsow15@gmail.com>
2023-05-07 21:02:51 +0200

    added VendorAdmin class inside the vendor/admin

--- vendor/admin.py
+++ vendor/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from vendor.models import Vendor
 
+
+class VendorAdmin(admin.ModelAdmin):
+    """Custom admin interface for the Vendor model."""
+    # The fields to display in the list view of vendors in the admin interface.
+    list_display = ('user', 'vendor_name', 'is_approved', 'created_at')
+    # The fields in the list display that should link to the edit form for the corresponding vendor.
+    list_display_links = ('user', 'vendor_name')
+
+
 admin.site.register(Vendor)
