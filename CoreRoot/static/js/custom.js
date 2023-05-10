84e39b52a211eb5ae09073b30d47132bb974253a
Shved15 <maks.shihiwtsow15@gmail.com>
2023-05-10 13:51:06 +0200

    added filling address components by google API for filed inside the vendor profile page

--- CoreRoot/static/js/custom.js
+++ CoreRoot/static/js/custom.js
@@ -44,4 +44,27 @@ function onPlaceChanged (){
     });
 
     // loop through the address components and assign other address data
+    console.log(place.address_components);
+    for(var i=0; i<place.address_components.length; i++){
+        for(var j=0; j<place.address_components[i].types.length; j++){
+            // get country
+            if(place.address_components[i].types[j] == 'country'){
+                $('#id_country').val(place.address_components[i].long_name);
+            }
+            // get state
+            if(place.address_components[i].types[j] == 'administrative_area_level_1'){
+                $('#id_state').val(place.address_components[i].long_name);
+            }
+            // get city
+            if(place.address_components[i].types[j] == 'locality'){
+                $('#id_city').val(place.address_components[i].long_name);
+            }
+            // get postal_code
+            if(place.address_components[i].types[j] == 'postal_code'){
+                $('#id_pin_code').val(place.address_components[i].long_name);
+            } else {
+                $('#id_pin_code').val('');
+            }
+        }
+    }
 }
