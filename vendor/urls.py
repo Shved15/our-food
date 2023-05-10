cc5a2ed4894099f0a731532972015a421073c57f
Shved15 <maks.shihiwtsow15@gmail.com>
2023-05-10 16:32:04 +0200

    added separate page for category of products and edited views for get categories and pass it in the context

--- vendor/urls.py
+++ vendor/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from accounts.views import vendor_dashboard
-from vendor.views import vendor_profile, catalog_builder
+from vendor.views import vendor_profile, catalog_builder, product_items_by_category
 
 urlpatterns = [
     path('', vendor_dashboard, name='vendor'),
     path('profile/', vendor_profile, name='vendor_profile'),
-    path('catalog-builder', catalog_builder, name='catalog_builder')
+    path('catalog-builder/', catalog_builder, name='catalog_builder'),
+    path('catalog-builder/category/<int:pk>/', product_items_by_category, name='product_items_by_category'),
 ]
--- vendor/views.py
+++ vendor/views.py
@@ -5,9 +5,14 @@ from django.shortcuts import render, get_object_or_404, redirect
 from accounts.forms import UserProfileForm
 from accounts.models import UserProfile
 from accounts.views import check_role_vendor
-from catalog.models import Category
+from catalog.models import Category, FoodItem
 from vendor.forms import VendorForm
 from vendor.models import Vendor
+
+
+def get_vendor(request):
+    vendor = Vendor.objects.get(user=request.user)
+    return vendor
 
 
 @login_required(login_url='login')
@@ -40,10 +45,25 @@ def vendor_profile(request):
     return render(request, 'vendor/vendor-profile.html', context)
 
 
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 def catalog_builder(request):
-    vendor = Vendor.objects.get(user=request.user)
+    vendor = get_vendor(request)
     categories = Category.objects.filter(vendor=vendor)
     context = {
         'categories': categories
     }
     return render(request, 'vendor/catalog-builder.html', context)
+
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def product_items_by_category(request, pk=None):
+    vendor = get_vendor(request)
+    category = get_object_or_404(Category, pk=pk)
+    product_items = FoodItem.objects.filter(vendor=vendor, category=category)
+    context = {
+        'product_items': product_items,
+        'category': category,
+    }
+    return render(request, 'vendor/product-items-by-category.html', context)
