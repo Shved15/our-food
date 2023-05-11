fc18b4ccbab45476900c94222157790a63304820
Shved15 <maks.shihiwtsow15@gmail.com>
2023-05-11 18:20:06 +0200

    added categories and products to the vendor details page

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,5 +1,7 @@
+from django.db.models import Prefetch
 from django.shortcuts import render, get_object_or_404
 
+from catalog.models import Category, FoodItem
 from vendor.models import Vendor
 
 
@@ -15,7 +17,14 @@ def marketplace(request):
 
 def vendor_detail(request, vendor_slug):
     vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)
+    categories = Category.objects.filter(vendor=vendor).prefetch_related(
+        Prefetch(
+            'food_items',
+            queryset=FoodItem.objects.filter(is_available=True)
+        )
+    )
     context = {
         'vendor': vendor,
+        'categories': categories,
     }
     return render(request, 'marketplace/vendor-details.html', context)
