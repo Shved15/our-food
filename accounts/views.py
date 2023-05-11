d06ceb818e081ad66958e6cbd3f42c34bba538c4
Shved15 <maks.shihiwtsow15@gmail.com>
2023-05-11 11:46:15 +0200

    fixed problems with register vendor

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.tokens import default_token_generator
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.core.exceptions import PermissionDenied
+from django.template.defaultfilters import slugify
 from django.utils.http import urlsafe_base64_decode
 
 from accounts.forms import UserForm
@@ -84,6 +85,8 @@ def register_vendor(request):
             user.save()
             vendor = vendor_form.save(commit=False)
             vendor.user = user
+            vendor_name = vendor_form.cleaned_data['vendor_name']
+            vendor.vendor_slug = slugify(vendor_name) + '-' + str(user.id)
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
