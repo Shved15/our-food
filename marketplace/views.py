c11dd78a11d90e30905679a5afb1d10ab5400ef4
Shved15 <maks.shihiwtsow15@gmail.com>
2023-05-11 21:22:57 +0200

    add logic add_to_cart with ajax to the console

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,8 +1,9 @@
 from django.db.models import Prefetch
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Category, FoodItem
+from marketplace.models import Cart
 from vendor.models import Vendor
 
 
@@ -32,4 +33,24 @@ def vendor_detail(request, vendor_slug):
 
 
 def add_to_cart(request, product_id):
-    return HttpResponse(product_id)
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            # Check if the food item exists
+            try:
+                product_item = FoodItem.objects.get(id=product_id)
+                # Check if the user has already added that food to the cart.
+                try:
+                    check_cart = Cart.objects.get(user=request.user, product_item=product_item)
+                    # Increase the cart quantity
+                    check_cart.quantity += 1
+                    check_cart.save()
+                    return JsonResponse({'status': 'Success', 'message': 'Increased the cart quantity'})
+                except:
+                    check_cart = Cart.objects.create(user=request.user, product_item=product_item, quantity=1)
+                    return JsonResponse({'status': 'Success', 'message': 'Added the product to the cart'})
+            except:
+                return JsonResponse({'status': 'Failed', 'message': 'This product does not exist!'})
+        else:
+            return JsonResponse({'status': 'Failed', 'message': 'Invalid request!'})
+    else:
+        return JsonResponse({'status': 'login_required', 'message': 'Please login to continue'})
