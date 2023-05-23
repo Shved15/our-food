8f1a14357f055f2f3968a73d627fb3357b9b7c5c
Shved15 <maks.shihiwtsow15@gmail.com>
2023-05-23 16:23:45 +0200

    changed from FBV to CBV customer profile logic

--- customer/urls.py
+++ customer/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 from accounts.views import CustomerDashboardView
-from customer.views import customer_profile, my_orders, order_detail
+from customer.views import CustomerProfileView, my_orders, order_detail
 
 urlpatterns = [
     path('', CustomerDashboardView.as_view(), name='customer'),
-    path('profile/', customer_profile, name='customer_profile'),
+    path('profile/', CustomerProfileView.as_view(), name='customer_profile'),
     path('my-orders/', my_orders, name='customer_my_orders'),
     path('order-detail/<int:order_number>/', order_detail, name='customer_order_detail'),
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,37 +1,64 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
+from django.views.generic import UpdateView
 
 from accounts.forms import UserProfileForm, UserInfoForm
 from accounts.models import UserProfile
+from accounts.views import check_role_customer
 from orders.models import Order, OrderedProduct
 
 import simplejson as json
 
 
-@login_required(login_url='login')
-def customer_profile(request):
-    profile = get_object_or_404(UserProfile, user=request.user)
-    if request.method == 'POST':
+class CustomerProfileView(LoginRequiredMixin, UserPassesTestMixin, View):
+    """Class-based view for customer profile."""
+
+    login_url = 'login'
+    template_name = 'customer/customer-profile.html'
+    success_url = 'customer_profile'
+    login_url = 'login'
+
+    def test_func(self):
+        """Check if the user passes the test for vendor role."""
+        return check_role_customer(self.request.user)
+
+    def get(self, request):
+        """Handle GET requests."""
+        profile = get_object_or_404(UserProfile, user=request.user)
+        profile_form = UserProfileForm(instance=profile)
+        user_form = UserInfoForm(instance=request.user)
+        context = {
+            'profile_form': profile_form,
+            'user_form': user_form,
+            'profile': profile,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        """Handle POST requests."""
+        profile = get_object_or_404(UserProfile, user=request.user)
         profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
         user_form = UserInfoForm(request.POST, instance=request.user)
+
         if profile_form.is_valid() and user_form.is_valid():
+            # Save the forms if they are valid
             profile_form.save()
             user_form.save()
             messages.success(request, 'Profile updated')
-            return redirect('customer_profile')
+            return redirect(self.success_url)
         else:
+            # Handle form validation errors
             print(profile_form.errors)
             print(user_form.errors)
-    else:
-        profile_form = UserProfileForm(instance=profile)
-        user_form = UserInfoForm(instance=request.user)
-    context = {
-        'profile_form': profile_form,
-        'user_form': user_form,
-        'profile': profile,
-    }
-    return render(request, 'customer/customer-profile.html', context)
+            context = {
+                'profile_form': profile_form,
+                'user_form': user_form,
+                'profile': profile,
+            }
+            return render(request, self.template_name, context)
 
 
 def my_orders(request):
