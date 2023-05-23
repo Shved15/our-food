d393608806d319d84d427a5fc8c5120e6534ed57
Shved15 <maks.shihiwtsow15@gmail.com>
2023-05-23 11:35:17 +0200

    fixed rendering to customer order details from dashboard and my orders

--- customer/urls.py
+++ customer/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', CustomerDashboardView.as_view(), name='customer'),
     path('profile/', customer_profile, name='customer_profile'),
     path('my-orders/', my_orders, name='customer_my_orders'),
-    path('order-detail/<int:order_number>/', order_detail, name='order_detail'),
+    path('order-detail/<int:order_number>/', order_detail, name='customer_order_detail'),
 ]
